985554f463c1961ce10393c6fef0ab4b5e967e3c
Serhii_Sych <Serhii_Sych@epam.com>
2019-08-08 16:59:14 +0300

    lambda connect to RDS

---
+++ lambda.py
@@ -0,0 +1,43 @@
+import sys
+import logging
+import pymysql
+
+# rds settings
+rds_host = "mysqlforlambdatest.ct0xb8em3m2k.eu-central-1.rds.amazonaws.com"
+username = "postgres"
+password = "postgres"
+name = "awstest"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(rds_host, user=username, passwd=password, db=name, connect_timeout=2)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    logger.error(e)
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+
+
+def handler(event, context):
+    """
+    This function fetches content from MySQL RDS instance
+    """
+
+    item_count = 0
+
+    with conn.cursor() as cur:
+        cur.execute("create table Employee ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))")
+        cur.execute('insert into Employee (EmpID, Name) values(1, "Joe")')
+        cur.execute('insert into Employee (EmpID, Name) values(2, "Bob")')
+        cur.execute('insert into Employee (EmpID, Name) values(3, "Mary")')
+        conn.commit()
+        cur.execute("select * from Employee")
+        for row in cur:
+            item_count += 1
+            logger.info(row)
+            # print(row)
+    conn.commit()
+
+    return "Added %d items from RDS MySQL table" % (item_count)
