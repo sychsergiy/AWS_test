42e987564108628d059042f3aa542025c56f943e
Serhii_Sych <Serhii_Sych@epam.com>
2019-08-12 13:25:56 +0300

    handle exceptions

--- lambda.py
+++ lambda.py
@@ -1,7 +1,8 @@
-import sys
 import logging
 import pymysql
 import boto3
+import time
+import uuid
 
 rds_host = "mysqlforlambdatest.ct0xb8em3m2k.eu-central-1.rds.amazonaws.com"
 username = "postgres"
@@ -12,6 +13,52 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+class LambdaExecutionStatuses(object):
+    INITIALIZATION = "INITIALIZATION"
+    FAILED_TO_INIT = "FAILED_TO_INIT"
+    IN_PROGRESS = "IN_PROGRESS"
+    FAILED = "FAILED"
+    SUCCESS = "SUCCESS"
+
+
+def create_lambda_status_record(table, lambda_name, status):
+    record_id = str(uuid.uuid4())
+
+    now = int(time.time())
+    data = {
+        "lambda_name": lambda_name,
+        "date_started": now,
+        "execution_status": status,
+        "uuid": record_id,
+    }
+    return table.put_item(Item=data), record_id
+
+
+def update_record_status(table, status, lambda_name, record_id):
+    response = table.update_item(
+        Key={
+            'lambda_name': lambda_name,
+            'uuid': record_id,
+        },
+        UpdateExpression="set execution_status = :r",
+        ExpressionAttributeValues={':r': status},
+        ReturnValues="UPDATED_NEW"
+    )
+    return response
+
+
+def init_dynamo_db_table():
+    try:
+        table_name = "LambdaExecutionState"
+        dynamo_db = boto3.resource("dynamodb")
+        table = dynamo_db.Table(table_name)
+        return table
+    except Exception as e:
+        logger.error("ERROR: Failed to init DynamoDB")
+        logger.error(e)
+        return None
+
+
 def init_rds():
     try:
         connection = pymysql.connect(rds_host, user=username, passwd=password, db=name, connect_timeout=2)
@@ -20,15 +67,11 @@ def init_rds():
     except pymysql.MySQLError as e:
         logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
         logger.error(e)
-        sys.exit()
+        return None
 
 
-def init_sns():
+def send_message_to_email_topic():
     sns = boto3.client('sns')
-    return sns
-
-
-def send_message_to_email_topic(sns):
     response = sns.publish(
         TopicArn='arn:aws:sns:eu-central-1:197928842860:email_lambda_finished',
         Message='Hello World!',
@@ -36,36 +79,64 @@ def send_message_to_email_topic(sns):
     return response
 
 
+def create_record_table(connection):
+    with connection.cursor() as cursor:
+        create_record_table_query = """
+            create table if not exists Record(
+                id  int NOT NULL AUTO_INCREMENT,
+                bucket_name varchar (255) NOT NULL,
+                updated_file_name varchar (255) NOT NULL ,
+                date_created datetime DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (id)
+            )
+            """
+        cursor.execute(create_record_table_query)
+    connection.commit()
+
+
+def insert_record_to_rds(cursor, bucket_name, updated_file_name):
+    cursor.execute(
+        'insert into Record (bucket_name, updated_file_name) values("%(bucket_name)s", "%(updated_file_name)s")',
+        {"bucket_name": bucket_name, "updated_file_name": updated_file_name}
+    )
+
+
 def handler(event, context):
-    conn = init_rds()
-    sns = init_sns()
+    lambda_name = "aws_test"
+    dynamo_db_table = init_dynamo_db_table()
+    if not dynamo_db_table:
+        return
+    _, record_id = create_lambda_status_record(
+        dynamo_db_table, lambda_name, LambdaExecutionStatuses.INITIALIZATION
+    )
+
+    rds_connection = init_rds()
+    if not rds_connection:
+        create_lambda_status_record(dynamo_db_table, lambda_name, LambdaExecutionStatuses.FAILED_TO_INIT)
+        return
+
+    update_record_status(
+        dynamo_db_table, LambdaExecutionStatuses.IN_PROGRESS, lambda_name, record_id
+    )
 
     source_s3 = event['Records'][0]['s3']
-    source_bucket_name = source_s3['bucket']['name']
-    updated_file_name = source_s3['object']['key']
+    source_bucket_name, updated_file_name = source_s3['bucket']['name'], source_s3['object']['key']
 
-    updated_file_text = "some text"
-    with conn.cursor() as cur:
-        create_record_table_query = """
-        create table if not exists Record(
-            id  int NOT NULL AUTO_INCREMENT,
-            bucket_name varchar (255) NOT NULL,
-            updated_file_name varchar (255) NOT NULL ,
-            date_created datetime DEFAULT CURRENT_TIMESTAMP,
-            PRIMARY KEY (id)
+    with rds_connection.cursor() as cursor:
+        insert_record_to_rds(cursor, source_bucket_name, updated_file_name)
+        rds_connection.commit()
+
+    try:
+        response = send_message_to_email_topic()
+        logger.info(response)
+        update_record_status(
+            dynamo_db_table, LambdaExecutionStatuses.SUCCESS, lambda_name, record_id
         )
-        """
-        cur.execute(create_record_table_query)
-        cur.execute(
-            'insert into Record (bucket_name, updated_file_name) values("%(bucket_name)s", "%(updated_file_name)s")',
-            {"bucket_name": source_bucket_name, "updated_file_name": updated_file_name}
+    except Exception as e:
+        logger.error("ERROR: Failed to send email message")
+        logger.error(e)
+        update_record_status(
+            dynamo_db_table, LambdaExecutionStatuses.FAILED, lambda_name, record_id
         )
-        cur.execute("select * from Record")
-        for row in cur:
-            logger.info(row)
-    conn.commit()
 
-    response = send_message_to_email_topic(sns)
-    logger.info(response)
-
-    return "Inserted record with text: %s" % updated_file_text
+    return "Inserted record with updated_file_name: %s" % updated_file_name
