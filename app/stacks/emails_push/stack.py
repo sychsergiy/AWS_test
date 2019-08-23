2e9ccdbe6a964caa85dcecc31e4b35852201cfd8
sychsergiy <sychsergiy@gmail.com>
2019-08-23 12:11:04 +0300

    emails sns topic output added

--- app/stacks/emails_push/stack.py
+++ app/stacks/emails_push/stack.py
@@ -1,6 +1,10 @@
 from troposphere import Template
 
+from stacks.parameters import email_address
 from stacks.emails_push.resources import emails_sns_topic
+from stacks.emails_push.outputs import emails_sns_topic_ref_output
 
 template = Template()
-template.add_parameter(emails_sns_topic)
+template.add_parameter(email_address)
+template.add_resource(emails_sns_topic)
+template.add_output(emails_sns_topic_ref_output)
